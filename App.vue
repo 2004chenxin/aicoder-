3f2e207ed0eb2142f0fde15aa091a66e7f7a1bbe
2004chenxin <abc2996277278@qq.com>
2026-06-30 11:39:28 +0800

    add App.vue

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>电影列表</h1>
+    <ul v-for="movie in movies" :key="movie.id">
+      <li>{{ movie.title }} - {{ movie.year }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      movies: []
+    }
+  },
+  created() {
+    this.$axios.get('/api/movies')
+      .then(response => {
+        this.movies = response.data;
+      });
+  }
+}
+</script>
